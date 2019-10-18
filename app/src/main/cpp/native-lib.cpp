18759147f1b784e2597aab1c3f63a753b1f45814
“pedroangelalvarez” <pedroangel.alvarez24@gmail.com>
2019-10-18 16:45:06 -0500

    Initial NDK

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,11 @@
 #include <jni.h>
 #include <string>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+
+using namespace std;
+using namespace cv;
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_aim_pesame_MainActivity_stringFromJNI(
